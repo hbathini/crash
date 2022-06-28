a82c10388a9a6b1c167cec2fadaa6834eae569fe
Hari Bathini <hbathini@linux.ibm.com>
2022-06-28 11:23:33 +0530

    ppc64: print emergency stacks info with 'mach -e'
    Add '-e' option to 'mach' command to list top address of all emergency
    stacks of all present CPUs.
    
    Signed-off-by: Hari Bathini <hbathini@linux.ibm.com>

--- ppc64.c
+++ ppc64.c
@@ -3153,6 +3153,44 @@ opalmsg(void)
 		fprintf(fp, "\n");
 }
 
+static void ppc64_print_emergency_stack_info(void)
+{
+	struct machine_specific *ms = machdep->machspec;
+	char buf[32];
+	int i;
+
+	fprintf(fp, "    EMERGENCY STACK: ");
+	if (ms->emergency_sp) {
+		fprintf(fp, "\n");
+		for (i = 0; i < kt->cpus; i++) {
+			sprintf(buf, "CPU %d", i);
+			fprintf(fp, "%19s: %lx\n", buf, ms->emergency_sp[i]);
+		}
+	} else
+		fprintf(fp, "(unused)\n");
+
+	fprintf(fp, "NMI EMERGENCY STACK: ");
+	if (ms->nmi_emergency_sp) {
+		fprintf(fp, "\n");
+		for (i = 0; i < kt->cpus; i++) {
+			sprintf(buf, "CPU %d", i);
+			fprintf(fp, "%19s: %lx\n", buf, ms->nmi_emergency_sp[i]);
+		}
+	} else
+		fprintf(fp, "(unused)\n");
+
+	fprintf(fp, " MC EMERGENCY STACK: ");
+	if (ms->mc_emergency_sp) {
+		fprintf(fp, "\n");
+		for (i = 0; i < kt->cpus; i++) {
+			sprintf(buf, "CPU %d", i);
+			fprintf(fp, "%19s: %lx\n", buf, ms->mc_emergency_sp[i]);
+		}
+	} else
+		fprintf(fp, "(unused)\n");
+	fprintf(fp, "\n");
+}
+
 /*
  *  Machine dependent command.
  */
@@ -3161,7 +3199,7 @@ ppc64_cmd_mach(void)
 {
         int c;
 
-	while ((c = getopt(argcnt, args, "cmo")) != EOF) {
+	while ((c = getopt(argcnt, args, "cmoe")) != EOF) {
                 switch(c)
                 {
 		case 'c':
@@ -3171,6 +3209,9 @@ ppc64_cmd_mach(void)
 			break;
 		case 'o':
 			return opalmsg();
+		case 'e':
+			ppc64_print_emergency_stack_info();
+			break;
                 default:
                         argerrs++;
                         break;
